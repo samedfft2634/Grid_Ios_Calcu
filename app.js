612396be5056c5e8cbd9d63cfdce2c94a6ac88a7
samedfft2634 <samedfft@gmail.com>
2023-10-20 18:08:02 +0300

    little but important

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js
-const allClear = document.querySelector(".number1")
+const allClear = document.querySelector(".number1");
 const resultElement = document.querySelector(".result");
-const resultPre = document.querySelector(".result-pre")
+const resultPre = document.querySelector(".result-pre");
 const numberContainer = document.querySelector(".number-container");
 
 let currentCalculation = "";
@@ -11,28 +11,29 @@ numberContainer.addEventListener("click", (event) => {
 	if (event.target.classList.contains("number")) {
 		const buttonValue = event.target.textContent;
 
-		if (buttonValue === "=") {
-			if (resultElement.value === "") {
+		if (buttonValue === "=" || buttonValue === "AC" || buttonValue === "DEL") {
+			if (resultElement.value === "" && buttonValue === "=") {
 				alert("Please enter a number");
+			} else if (buttonValue === "AC") {
+				currentCalculation = "";
+				resultElement.value = "";
+			} else if (buttonValue === "DEL") {
+				currentCalculation = currentCalculation.toString().slice(0, -1);
+                resultElement.value = currentCalculation
 			} else {
 				currentCalculation = eval(currentCalculation);
-                resultPre.value = currentCalculation
-                resultElement.value = "";
-                
+				resultPre.value = currentCalculation;
+				resultElement.value = "";
 			}
 		} else if (buttonValue === "AC") {
 			currentCalculation = "";
 		} else if (buttonValue === "DEL") {
-			currentCalculation = currentCalculation.toString().slice(0,-1);
+			currentCalculation = currentCalculation.toString().slice(0, -1);
 		} else {
 			currentCalculation += buttonValue;
-            resultElement.value = currentCalculation;
+			resultElement.value = currentCalculation;
 		}
-      
-		
 	}
 });
 
-
-// enter 
-
+// enter
