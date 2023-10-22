73a8c4730560dbbb9aae4ded328b51e9f70a8eac
samedfft2634 <samedfft@gmail.com>
2023-10-22 16:04:54 +0300

    allow 1 dot

--- app.js
+++ app.js
@@ -9,24 +9,27 @@ const resultPre = document.querySelector(".result-pre");
 const numberContainer = document.querySelector(".number-container");
 
 let currentCalculation = "";
-let previousCalculation = "";
+let preCal = "";
+let dot = 1
 
 numberContainer.addEventListener("click", (event) => {
 	if (event.target.classList.contains("number")) {
 		const buttonValue = event.target.textContent;
-		
+
 		if (buttonValue === "=") {
-			const lastChar = currentCalculation.charAt(currentCalculation.length - 1);
+			const lastChar = currentCalculation.charAt(
+				currentCalculation.length - 1
+			);
 			if (resultElement.value === "") {
 				alert("Please enter a number");
-			} else if(isOperator(lastChar)){
-				currentCalculation = currentCalculation.slice(0, -1)
+			} else if (isOperator(lastChar)) {
+				currentCalculation = currentCalculation.slice(0, -1);
 				resultElement.value = currentCalculation;
-			}  else {
+			} else {
 				currentCalculation = eval(currentCalculation);
 				resultPre.value = currentCalculation;
 				resultElement.value = "";
-				currentCalculation = ""; 
+				currentCalculation = "";
 				// console.log(currentCalculation,typeof currentCalculation,resultPre.value, typeof resultPre, resultPre) // if you do not reset current , in 40.line will error cuz currentCalculation will be number.
 			}
 		} else if (buttonValue === "AC") {
@@ -37,30 +40,43 @@ numberContainer.addEventListener("click", (event) => {
 			currentCalculation = currentCalculation.toString().slice(0, -1);
 			resultElement.value = currentCalculation;
 		} else {
-			const lastChar = currentCalculation.charAt(currentCalculation.length - 1);
+			const lastChar = currentCalculation.charAt(
+				currentCalculation.length - 1
+			);
 			if (!isOperator(lastChar) && buttonValue === "%") {
-				// Son eklenen karakter operatör değilse ve bu sefer "%" butonuna basıldıysa
-				currentCalculation = (parseFloat(currentCalculation) / 100).toString();
+				// If the last character added is not an operator and this time the "%" button is pressed
+				currentCalculation = (
+					parseFloat(currentCalculation) / 100
+				).toString();
 				resultPre.value = currentCalculation;
 				resultElement.value = "";
 			} else if (!isOperator(lastChar) && isOperator(buttonValue)) {
-				// Son eklenen karakter operatör değilse ve bu sefer bir operatör ekleniyorsa
-				currentCalculation += buttonValue;
-				resultElement.value = currentCalculation;
+				//  If the last character added is not an operator and an operator is added this time
+				if (currentCalculation.includes(".") && buttonValue === ".") {
+					if(dot === 1 && buttonValue === "."){
+					resultElement.value = currentCalculation;
+					} 
+				} else {
+					currentCalculation += buttonValue;
+					resultElement.value = currentCalculation;
+				}
 			} else if (isOperator(lastChar) && isOperator(buttonValue)) {
-				// Son eklenen karakter operatör ise ve bu sefer de operatör ekleniyorsa
-				currentCalculation = currentCalculation.slice(0, -1) + buttonValue;
-				resultElement.value = currentCalculation;
-			} else {		
+				// If the last character added is an operator and this time an operator is added
+				if(buttonValue !== "."){
+					currentCalculation =currentCalculation.slice(0, -1) + buttonValue;
+					resultElement.value = currentCalculation;
+				}
+				
+			} else {
 				currentCalculation += buttonValue;
 				resultElement.value = currentCalculation;
-			}	
+			}
 		}
 	}
 });
 
 function isOperator(char) {
-	// Verilen karakter bir operatör mü kontrol eder.
+	// Checks if the given character is an operator.
 	return "+-*/.%".includes(char);
 }
 
@@ -79,16 +95,14 @@ resultElement.addEventListener("keydown", (event) => {
 			}
 		}
 	}
-	// resultElement.value = "" // bu buradayken bastigim her tustan sonra bir onceki kayboluyor
+	// resultElement.value = "" // every time I press a button while this is here, the previous one disappears.
 });
 
+
+// When the page opens and click on the image, the ios calculator will appear
 const resim = document.querySelector(".img1");
 resim.addEventListener("click", function () {
 	resim.style.display = "none";
 });
 
 resim.style.display = "none";
-
-// else if (currentCalculation.includes(".")){
-
-// }
